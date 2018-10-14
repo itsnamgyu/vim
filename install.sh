80d1ad0ba6b6e242815a2921afe2e0567986fa7a
Namgyu Ho <itsnamgyu@gmail.com>
2018-10-15 00:22:00 +0900

    adopt install + clean interface
    + from the bash repo

---
+++ install.sh
@@ -0,0 +1,7 @@
+mv ~/.vimrc ~/.vimrc.bck
+cat vimrc > ~/.vimrc
+
+rm -rf ~/.vim
+cp -r vim ~/.vim
+
+source clean.sh
--- makefile
+++
@@ -1,7 +0,0 @@
-default:
-	cat vimrc > ~/.vimrc
-	-rm -rf ~/.vim
-	cp -r vim ~/.vim
-
-clean:
-	rm -rf `pwd`
